14798be16bcda049a1e9a395bc3b6ecbcde76a76
Christoffer Piilmann <christoffer@korn8.dk>
2018-10-17 11:49:56 +0200

    Update BackendWCF/BackendWCF/Event.cs

---
+++ BackendWCF/BackendWCF/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendWCF
+{
+    public class Event
+    {
+        String navn { get; set; }
+        String beskrivelse { get; set; }
+        String tidspunkt { get; set; }
+        String sted { get; set; }
+    }
+}
