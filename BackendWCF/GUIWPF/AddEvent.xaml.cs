717c5d401fb3360c63cdd00d62ca6ba755f457da
sofiefreja <spiegel1>
2018-10-31 14:31:27 +0100

    Client startup

--- BackendWCF/GUIWPF/AddEvent.xaml.cs
+++ BackendWCF/GUIWPF/AddEvent.xaml.cs
@@ -12,17 +12,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GUIWPF.ServiceReference1;
 
 namespace GUIWPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddEvent.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class AddEvent : Page
     {
-        public MainWindow()
+        public AddEvent()
         {
             InitializeComponent();
         }
+        // Custom constructor to pass expense report data
+        public AddEvent(object data):this()
+{
+            // Bind to expense report data.
+            this.DataContext = data;
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+       
+        
     }
 }
---
+++ BackendWCF/GUIWPF/EventList.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GUIWPF.ServiceReference1;
+
+namespace GUIWPF
+{
+    /// <summary>
+    /// Interaction logic for EventList.xaml
+    /// </summary>
+    public partial class EventList : Page
+    {
+        public EventList()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            AddEvent AddEvent = new AddEvent(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(AddEvent);
+        }
+        private void getData()
+        {
+            Service1Client client = new Service1Client();
+            client.GetAllEvents();
+        }
+
+
+    }
+}
