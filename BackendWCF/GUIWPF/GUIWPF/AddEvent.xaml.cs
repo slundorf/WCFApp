a06b7c4ce0d05d090cb704c10e50e3aab550b7ff
Christoffer Piilmann <christoffer@korn8.dk>
2018-11-14 11:34:37 +0100

    Added GUIWPF

---
+++ BackendWCF/GUIWPF/GUIWPF/AddEvent.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GUIWPF.ServiceReference1;
+
+namespace GUIWPF
+{
+    /// <summary>
+    /// Interaction logic for AddEvent.xaml
+    /// </summary>
+    public partial class AddEvent : Page
+    {
+        public AddEvent()
+        {
+            InitializeComponent();
+        }
+        // Custom constructor to pass expense report data
+        public AddEvent(object data):this()
+{
+            // Bind to expense report data.
+            this.DataContext = data;
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+       
+        
+    }
+}
