7af59ff2c0c744da8f56817297ba516e0ef1dced
Christoffer Piilmann <christoffer@korn8.dk>
2018-11-14 09:54:12 +0100

    Start backend console app

---
+++ BackendWCF/StartBackend/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using BackendWCF;
+namespace StartBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1 Create a URI to serve as the base address.
+            Uri baseAddress = new Uri("http://localhost:8584/DTU");
+
+            // Step 2 Create a ServiceHost instance
+            ServiceHost selfHost = new ServiceHost(typeof(Service1), baseAddress);
+
+            try
+            {
+                // Step 3 Add a service endpoint.
+                selfHost.AddServiceEndpoint(typeof(IService1), new WSHttpBinding(), "CalculatorService");
+
+                // Step 4 Enable metadata exchange.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                // Step 5 Start the service.
+                selfHost.Open();
+                Console.WriteLine("Web service er deployed.");
+                Console.WriteLine("Tryk <ENTER> for at terminere web service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                // Close the ServiceHostBase to shutdown the service.
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
