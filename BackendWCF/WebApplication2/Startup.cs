a69dcb3e57e6ed3df3e85be6748ebb758d352883
Simon Lundorf <s154008@win.dtu.dk>
2018-12-13 20:37:45 +0100

    Færddiggjort web klienten

--- BackendWCF/WebApplication2/Startup.cs
+++ BackendWCF/WebApplication2/Startup.cs
@@ -56,7 +56,7 @@ namespace WebApplication2
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Event}/{id?}");
             });
         }
     }
